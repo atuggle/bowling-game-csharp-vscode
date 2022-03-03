1c9944c97dfc8b899dfa20d920e2ead943e148ff
Tuggle, Allen W <allen.tuggle@ge.com>
2022-03-02 23:14:13 -0500

    project creation and initial test created

---
+++ bowlinggame.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using bowlinggame.kata;
+
+namespace bowlinggame.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CanCreateGame()
+    {
+        //Arrange
+        var game = new Game();
+
+        //Act
+
+        //Assert
+    }
+}
